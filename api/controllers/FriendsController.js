a46fa2b2ed06e0543a3eae6c9598ba30ace5e81e
CristhianMotoche <cristhian.motoche@gmail.com>
2017-08-14 22:01:04 -0500

    Get data from cookie session

--- api/controllers/FriendsController.js
+++ api/controllers/FriendsController.js
@@ -6,7 +6,8 @@
 
 module.exports = {
   getFriends: function (req, res) {
-    var userId = req.param('userId');
+    console.log(req.user);
+    var userId = req.user.id;
     return res.send({
       message: "Correct! " + userId
     });
